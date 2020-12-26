5b3872700179f900ce63f9f9ec92e6517733e185
Todd Bensmiller <toddbjta@gmail.com>
2020-12-25 18:47:18 -0600

    fixed simulation blank screen on launch

--- src/main/kotlin/me/toddbensmiller/sirvisual/Main.kt
+++ src/main/kotlin/me/toddbensmiller/sirvisual/Main.kt
@@ -4,7 +4,6 @@ import me.toddbensmiller.sirvisual.gui.SIRGUIApp
 import tornadofx.*
 import com.natpryce.konfig.*
 import javafx.scene.image.Image
-import javafx.scene.image.WritableImage
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
@@ -16,18 +15,20 @@ import kotlin.random.Random.Default.nextInt
 
 fun main(args: Array<String>)
 {
-	// define settings to be imported
+
 	val sToIRate = Key("infectionRate", doubleType)
 	val iToRRate = Key("recoveryRate", doubleType)
 	val rToSRate = Key("relapseRate", doubleType)
 	val isSIRS = Key("isSIRS", booleanType)
 	val neighborRadius = Key("radius", intType)
 	val initial = Key("initial", intType)
-// import the settings
+
 	val config = ConfigurationProperties.fromFile(File("sirs.properties")) overriding
 			ConfigurationProperties.fromResource("default.properties")
-	// initilize the SIR model using the imported or default settings
+
 	SIRModel.init(450, config[neighborRadius], config[sToIRate], config[iToRRate], config[rToSRate], config[isSIRS], config[initial])
+
+	SIRModel.setStateOfCell(nextInt(300) + 50, nextInt(300) + 50, SIRState.INFECTED)
 	GlobalScope.launch { SIRModel.reset() }
 	launch<SIRGUIApp>(args)
 
