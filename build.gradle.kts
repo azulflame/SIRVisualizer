0c3d85e6fddcdaf710aac7bbf796c8640e845558
= <toddbjta@gmail.com>
2021-03-08 01:03:43 -0600

    changed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,15 @@ application {
 }
 
 group = "me.toddbensmiller.sirvisual"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
 	maven("https://www.jitpack.io")
 	mavenCentral()
 }
 dependencies {
-	// imgui deps
 	implementation("no.tornado:tornadofx:1.7.20")
-	implementation("com.natpryce:konfig:1.6.10.0")    // for the properties file
+	implementation("com.natpryce:konfig:1.6.10.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.30")
 }
